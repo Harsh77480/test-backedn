31f6c807b42cb34be62e5a8d8567a203d028bdf3
Harsh77480 <otherworks77480@gmail.com>
2023-10-27 19:51:28 +0530

    handle cors

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 dotenv.config();
+
+// Cross Origin Protection
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 const connectDB = require('./src/config/db');
 connectDB();
 app.use(express.json());
@@ -26,6 +39,7 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port,()=>{
     console.log("running " + port);
 });
+
 
 const io = require('socket.io')(server,{
     pingTimeout : 60000,                    //close the connection 
