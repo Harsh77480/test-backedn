044800244cbd3076c592292c6321663be9775fa3
Harsh77480 <otherworks77480@gmail.com>
2023-10-28 02:32:42 +0530

    cors handled 2

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const server = app.listen(port,()=>{
 const io = require('socket.io')(server,{
     pingTimeout : 60000,                    //close the connection 
     cors : {
-        origin : "http://localhost:5173",
+        origin : "*",
     }
 });
 
